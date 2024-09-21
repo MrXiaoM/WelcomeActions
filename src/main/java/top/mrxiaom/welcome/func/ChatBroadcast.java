57c73b7e25cafe0b63ab5a7d645c9b0a6b1cb556
人間工作 <coolxiaom95@gmail.com>
2024-09-22 02:59:07 +0800

    prevent run commands as chat

--- src/main/java/top/mrxiaom/welcome/func/ChatBroadcast.java
+++ src/main/java/top/mrxiaom/welcome/func/ChatBroadcast.java
@@ -6,6 +6,10 @@ import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import top.mrxiaom.pluginbase.func.AutoRegister;
 import top.mrxiaom.pluginbase.utils.Bytes;
 import top.mrxiaom.welcome.WelcomeActionsPlugin;
@@ -16,16 +20,29 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 @AutoRegister
-public class ChatBroadcast extends AbstractModule {
+public class ChatBroadcast extends AbstractModule implements Listener {
     private static final String CHANNEL = "WelcomeActionsChat";
     boolean receive;
     public ChatBroadcast(WelcomeActionsPlugin plugin) {
         super(plugin);
+        registerEvents();
     }
 
     @Override
     public void reloadConfig(MemoryConfiguration config) {
         receive = config.getBoolean("actions.receiver-welcome");
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerChat(AsyncPlayerChatEvent e) {
+        if (e.isCancelled()) return;
+
+        Player player = e.getPlayer();
+        if (e.getMessage().startsWith("/")) {
+            e.setCancelled(true);
+            String command = e.getMessage().substring(1);
+            Bukkit.getScheduler().runTask(plugin, () -> Bukkit.dispatchCommand(player, command));
+        }
     }
 
     public void broadcast(Player p, BaseComponent... msg) {
